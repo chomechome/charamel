7e4ccbfb6fe95f6b9c5db43520ba3bebd25ed706
chomechome <cunningplan@yandex.ru>
2020-05-10 16:40:14 +1000

    Update README

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -302,7 +302,7 @@ def create_encoding_accuracy_breakdown(hits: Dict[str, Dict[str, int]]) -> str:
             )
         breakdown.append(line)
 
-    return tabulate.tabulate(breakdown, headers, tablefmt='pretty')
+    return tabulate.tabulate(breakdown, headers, tablefmt='grid')
 
 
 def create_detector_metrics(times: Dict[str, List[float]], hits) -> str:
@@ -342,7 +342,7 @@ def create_detector_metrics(times: Dict[str, List[float]], hits) -> str:
             ]
         )
 
-    return tabulate.tabulate(breakdown, headers, tablefmt='pretty')
+    return tabulate.tabulate(breakdown, headers, tablefmt='grid')
 
 
 def main():
